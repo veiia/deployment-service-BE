f3e25f8c10db6ec2a66c5858cd59365294715e2a
syth0le <chdan565@gmail.com>
2023-03-23 11:00:16 +0300

    added makefile with right docker-compose file

--- app/__main__.py
+++ app/__main__.py
@@ -20,7 +20,5 @@ async def shutdown() -> None:
     pass
 
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "__main__:app", host="0.0.0.0", port=8088, log_level="info", reload=True
-    )
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8088, log_level='info', reload=True)
